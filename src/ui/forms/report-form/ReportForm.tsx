e09f41e7e74384f2817048db5e3eac0f1e302e9b
jonaspaulosulit <jonas_sulit2001@yahoo.com>
2025-01-23 15:10:31 -0800

    ability to search using vanity name or steam id

--- src/ui/forms/report-form/ReportForm.tsx
+++ src/ui/forms/report-form/ReportForm.tsx
@@ -1,46 +1,92 @@
 import { useState } from "react"
-import { getUserSteamProfile } from "../../../apis/steam/searchUser"
+import { getUserSteamId, getUserSteamProfile } from "../../../apis/steam/searchUser"
 
 
 type Player = {
     realname?: string
     profileurl?: string
     avatar?: string
+    personaname?: string
+
 }
 
 const ReportForm = () => {
  
   const [steamId, setSteamId] = useState<string>('')
+  const [vanityName, setVanityName] = useState<string>('')
   const [player, setPlayer] = useState<Player>({})
+  const [vanityOrSteamId, setVanityOrSteamId] = useState<string>('')
 
   const handleSearch = async (event: any) :Promise<void> => {
     event.preventDefault()
 
-    try {
-        const response = await getUserSteamProfile(steamId)
+    if(parseInt(vanityOrSteamId)) {//this will true if you provided the steam id granted it is all numbers
         
-        const { success, response: playerInfo  } = response
+        try {
+            const player = await getUserSteamProfile(vanityOrSteamId)
 
-        if (!success) {
-            alert('Error Fetching Player Info')
+            const { success, response: playerInfo  } = player
+
+            if(!success) {
+                alert('No Details found')
+                throw new Error("No Details Found")
+            }
+
+            setPlayer(playerInfo)
+
+        } catch (error) {
+            console.log(error)
         }
 
-        setPlayer(playerInfo)
-
-    } catch (error) {
-        console.log(error)
+    } else {
+        try {
+            const response = await getUserSteamId(vanityOrSteamId)
+    
+            const { success: successOnVanity, response: playerSteamId  } = response
+    
+    
+            if(!successOnVanity) {
+                alert('cannot find player')
+                throw new Error('Cannot Find Player')
+    
+            }
+    
+            const player = await getUserSteamProfile(playerSteamId)
+    
+            const { success, response: playerInfo  } = player
+    
+            if(!success) {
+                alert('No Details found')
+                throw new Error("No Details Found")
+                
+            }
+    
+            setPlayer(playerInfo)
+    
+        } catch (error) {
+            console.log(error)
+        }
     }
+
   }
 
   return (
     <>
     <form onSubmit={handleSearch}>
 
-    Enter Steam Id:
+    {/* Enter Steam Id:
     <input
     type="text"
     value={steamId}
     onChange={e => setSteamId(e.target.value)}
+    /> */}
+
+    Enter Player Vanity Name or Steam Id: <br />
+    <input
+     type="text"
+     name=""
+     value={vanityOrSteamId}
+     onChange={e => setVanityOrSteamId(e.target.value)}
     />
 
     <input type="submit" value="Search" />
@@ -48,7 +94,7 @@ const ReportForm = () => {
 
     <img src={player.avatar} alt=""/>
     {
-        player.realname
+        player.personaname
     }
     </>
     
