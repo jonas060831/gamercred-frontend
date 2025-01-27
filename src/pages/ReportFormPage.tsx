ec465095b61b41bc7b090281fbd48513a6044293
jonaspaulosulit <jonas_sulit2001@yahoo.com>
2025-01-26 16:26:53 -0800

    bug on state handling

--- src/pages/ReportFormPage.tsx
+++ src/pages/ReportFormPage.tsx
@@ -1,15 +1,125 @@
+import { useEffect, useState } from "react"
 import ReportForm from "../ui/forms/report-form/ReportForm"
 import MastHead from "../ui/masthead/MastHead"
+import PlayerLookUpForm from "../ui/forms/report-form/PlayerLookUpForm"
+import { getUserSteamId, getUserSteamProfile } from "../apis/steam/searchUser"
 
 const ReportFormPage = () => {
+
+  const [playerToReport, setplayerToReport] = useState({})
+  const [vanityOrSteamId, setVanityOrSteamId] = useState<any>('')
+
+  const handleSearch = async (event: any) :Promise<void> => {
+    event.preventDefault()
+
+    if(vanityOrSteamId === '') {
+        alert('input cannot be empty')
+    } else {
+        //if the input is string
+        if(isNaN(vanityOrSteamId)) {
+
+            try {
+                const response = await getUserSteamId(vanityOrSteamId)
+        
+                const { success: successOnVanity, response: playerSteamId  } = response
+        
+                
+                if(!successOnVanity) {
+                    console.log('no matching result found')
+                    setplayerToReport({ personaname: '42' })
+                    
+                }
+                
+                if(Object.keys(playerSteamId).length !== 0) {
+                    const player = await getUserSteamProfile(playerSteamId)
+        
+                    const { success, response: playerInfo  } = player
+            
+                    if(!success) {
+                        alert('No Details found')
+                        throw new Error("No Details Found")
+                        
+                    }
+
+                    if(playerInfo === undefined) {
+                        
+                        console.log('no player found')
+                        setplayerToReport({ personaname: '42' })
+                    }
+
+                    //account created successfully
+                    setplayerToReport(playerInfo)
+                } else {
+                    setplayerToReport({personaname: '42'})
+                }
+                        
+            } catch (error) {
+                console.log(error)
+            }
+
+        } else { //this will true if you provided the steam id granted it is all numbers
+
+            try {
+                const player = await getUserSteamProfile(vanityOrSteamId)
+
+                const { success, response: playerInfo  } = player
+
+                if(!success) {
+                    alert('No Details found')
+                    throw new Error("No Details Found")
+                }
+
+                setplayerToReport(playerInfo)
+
+            } catch (error) {
+                console.log(error)
+                console.log('no result found')
+            }
+        }
+    }
+  }
+
+  useEffect(() => {
+
+  }, [playerToReport])
+  
   return (
     <div className="page_container">
         
-        <MastHead header="Report Player" description="" height="auto">
-          <ReportForm />
+        <MastHead header="Player Lookup" description="" height="auto">
+            <PlayerLookUpForm
+             playerToReport={playerToReport}
+             handleSearch={handleSearch}
+             vanityOrSteamId={vanityOrSteamId}
+             setVanityOrSteamId={setVanityOrSteamId}
+            />
         </MastHead>
     </div>
   )
+
+  // if(Object.keys(playerToReport).length === 0) return (
+    
+  //   <div className="page_container">
+        
+  //       <MastHead header="Player Lookup" description="" height="auto">
+  //           <PlayerLookUpForm
+  //            playerToReport={playerToReport}
+  //            handleSearch={handleSearch}
+  //            vanityOrSteamId={vanityOrSteamId}
+  //            setVanityOrSteamId={setVanityOrSteamId}
+  //           />
+  //       </MastHead>
+  //   </div>
+  // )
+
+  // return (
+  //   <div className="page_container">
+        
+  //       <MastHead header="Report Player" description="" height="auto">
+  //         <ReportForm />
+  //       </MastHead>
+  //   </div>
+  // )
 }
 
 export default ReportFormPage
--- src/ui/forms/report-form/UserResult.tsx
+++ src/ui/forms/report-form/UserResult.tsx
@@ -1,18 +1,122 @@
-import { FC } from "react"
-import { Player } from "./ReportForm"
+import { FC, useEffect, useState } from "react"
+
+
+import { Player } from "./PlayerLookUpForm"
 
 type UserResultProps = {
     playerResult: Player
+    handleSearch?: () => void
 }
 
-const UserResult:FC<UserResultProps> = ({playerResult}) => {
-  return (
-    <div style={{ display: 'flex', flexDirection: 'column', gap: '1rem', alignItems: 'center', justifyContent: 'center' }}>
+import styles from './UserResult.module.css'
+import ButtonLink from "../../button-link/ButtonLink"
+import { fetchRecentGames } from "../../../apis/steam/searchUser"
 
-        <img src={playerResult?.avatar} alt="" style={{ width: '5rem', borderRadius: '100vh', border: '2px solid gray', objectFit: 'cover' }}/>
-        {
-            playerResult?.personaname
-        }
+const UserResult:FC<UserResultProps> = ({playerResult , handleSearch}) => {
+
+  const [gamesOwned, setGamesOwned] = useState([])
+  const [recentlyPlayed, setRecentlyPlayed] = useState([])
+  
+  useEffect(() => {
+
+    fetchRecentlyPlayedGames(playerResult.steamid)
+
+  },[])
+
+
+  const fetchRecentlyPlayedGames = async(playerId?: string) => {
+
+   const response:any = await fetchRecentGames(playerId!)
+
+
+   //forks to handle user not having any game and user having games
+   if(!response.data) console.log('No games yet')
+
+   else {
+    //definetly have games
+    console.log(response.data.response.games)
+    setRecentlyPlayed(response.data.response.games)
+
+   }
+
+  }
+
+  return (
+    <div
+     style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        gap: '1rem',
+        backgroundColor: 'black',
+        width: '600px',
+        padding: '1rem'
+     }}
+    >
+
+        {/* contains the left and right side uis */}
+        <div className={styles.left_and_right_ui}>
+
+            {/* contains useravatar and player name */}
+            <div className={styles.header}>
+
+                <div>
+                    <img
+                    src={playerResult.avatar} alt="avatar image"
+                    style={{ marginRight: '0.7rem' }}
+                    />
+                    <span>{ playerResult.personaname }</span>
+                </div>
+                
+                <div>
+                    recent games played: { recentlyPlayed.length } <br />
+                    
+
+                    {
+                        recentlyPlayed.length === 0 ? (
+                            <></>
+                        ) : (
+                            <>
+                                {
+                                    recentlyPlayed.map((game:any, index) => (
+
+
+                                        <div key={index} style={{ display: 'flex' }}>
+                                            <img src={`https://media.steampowered.com/steamcommunity/public/images/apps/${game.appid}/${game.img_icon_url}.jpg`} alt="" style={{ width: '2rem', height: '2rem' }}/>
+                                            {game.name} <br />
+                                        </div>
+                                    ))
+                                }
+                            </>
+                        )
+                    }
+
+                </div>
+
+                <button >Report Player</button>
+            </div>
+
+
+            {/* information section */}
+            <div>
+                
+                <ButtonLink to="/" title="Steam Link"/>
+                
+                <br />
+                
+                <div>
+                    Some Chart
+                </div>
+            </div>
+
+        </div>
+
+
+
+        {/* reputation bar */}
+        <div>
+            <progress value="32" max="100" style={{ width: '100%' }}>32%</progress>
+        </div>
 
     </div>
   )
